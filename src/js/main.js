5ddefb8f6d1409782739e64049a48539e6ae97a4
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2024-12-17 10:10:12 -0300

    feat: Added Slide Timeline
    Usando a lib Swiper.js

--- src/js/main.js
+++ src/js/main.js
@@ -23,3 +23,39 @@ document.addEventListener('DOMContentLoaded', function() {
     const currentYear = new Date().getFullYear();
     yearSpan.textContent = currentYear.toString();
 });
+
+
+// Swiper - Slide Timeline
+document.addEventListener('DOMContentLoaded', () => {
+    const swiperContainer = document.querySelector('.timeline .swiper-container');
+    if (swiperContainer) {
+        console.log("Inicializando o Swiper...");
+        const timelineSwiper = new Swiper('.timeline .swiper-container', {
+            direction: 'vertical',
+            loop: false,
+            speed: 1600,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+                renderBullet: function (index, className) {
+                    const year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+                    console.log(`Renderizando bullet: ${year}`); // Log para cada bullet
+                    return '<span class="' + className + '">' + year + '</span>';
+                },
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                768: {
+                    direction: 'horizontal',
+                },
+            },
+        });
+
+        console.log("Swiper inicializado com sucesso:", timelineSwiper);
+    } else {
+        console.error("Erro: Não foi possível inicializar o Swiper, pois o container não foi encontrado.");
+    }
+});
